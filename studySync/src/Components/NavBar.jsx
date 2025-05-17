fd6f6548a031a649a5fbe89599ce6e6c51388ce6
Amrita <amritakataria515@gmail.com>
2025-05-17 14:57:57 +0530

    drop down menu set

--- studySync/src/Components/NavBar.jsx
+++ studySync/src/Components/NavBar.jsx
@@ -1,22 +1,50 @@
-<div className="flex items-center justify-between p-5 pl-6">
-  <div>
-    <h1 className="text-2xl text-transparent bg-clip-text font-extrabold bg-gradient-to-r from-gray-100 via-gray-300 to-indigo-400">
-      StudySync
-    </h1>
-  </div>
+import { HiMenu } from "react-icons/hi";
+import { useState } from "react";
+// import { Turn as Hamburger } from 'hamburger-react'
 
-  {/* Desktop Links */}
-  <div className="hidden md:flex items-center justify-between gap-4">
-    <h4>Home</h4>
-    <h4>Features</h4>
-    <h4>Notes</h4>
-    <h4>Study Room</h4>
-    <h4>Doubts</h4>
-    <h4>Login / Sign Up</h4>
-  </div>
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+     <>
+        <div className="flex items-center justify-between p-5 pl-6">
+            <div>
+                <h1 className="text-2xl text-transparent bg-clip-text font-extrabold bg-gradient-to-r from-gray-100 via-gray-300 to-indigo-400">
+                    StudySync
+                </h1>
+            </div>
 
-  {/* Mobile Menu Icon */}
-  <div className="md:hidden">
-    <HiMenu className="text-4xl text-white" />
-  </div>
-</div>
+            {/* Desktop Links */}
+            <div className="hidden md:flex items-center justify-between gap-4">
+                <h4>Home</h4>
+                <h4>Features</h4>
+                <h4>Notes</h4>
+                <h4>Study Room</h4>
+                <h4>Doubts</h4>
+                <h4>Login / Sign Up</h4>
+            </div>
+
+            {/* Mobile Menu Icon */}
+            <div className="md:hidden">
+                <HiMenu onClick = {() => { console.log("button clicked") 
+                setMenuOpen(!menuOpen)
+                }} 
+                className="text-4xl text-white cursor-pointer" />
+
+                {menuOpen && (
+                    <div>
+                            <h4>Home</h4>
+                            <h4>Features</h4>
+                            <h4>Notes</h4>
+                            <h4>Study Room</h4>
+                            <h4>Doubts</h4>
+                            <h4>Login / Sign Up</h4> 
+                    </div>
+                )}
+            </div>
+
+        </div>
+    </>
+    );
+};
+
+export default NavBar;
