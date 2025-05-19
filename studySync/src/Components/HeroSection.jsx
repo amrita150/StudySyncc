0700bbbfe3ae5534756d7a025b7d7581e94f5684
Amrita <amritakataria515@gmail.com>
2025-05-19 15:40:56 +0530

    navbar responsive and style hero section

--- studySync/src/Components/HeroSection.jsx
+++ studySync/src/Components/HeroSection.jsx
@@ -1,12 +1,12 @@
 const HeroSection = () =>{
     return(
         <div className="flex justify-center items-center flex-col">
-            <div>
-                <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-l from-blue-500 via-purple-500 to-pink-500 ">
-                    THE FUTURE of Group Study</h1>
+            <div className="flex justify-center items-center pb-3">
+                <h1 className="text-6xl ml-40 mr-40 justify-center text-center font-bold text-transparent bg-clip-text bg-gradient-to-l from-blue-500 via-purple-500 to-pink-500">
+                    THE FUTURE of <br/>Group Study</h1>
             </div>
-            <div>
-                <p className="text-2xl">AI-powered study rooms, verified notes, and more, designed for seamless collaboration.</p>
+            <div className="p-3">
+                <p className="text-xl text-center">AI-powered study rooms, verified notes, <br /> and more, designed for seamless collaboration.</p>
             </div>
             <div>
                 <button className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded pr-7 pl-7 text-2xl">Join Now</button>
--- studySync/src/Components/NavBar.jsx
+++ studySync/src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { HiMenu } from "react-icons/hi";
 import { useState } from "react";
-// import { Turn as Hamburger } from 'hamburger-react'
+import { Turn as Hamburger } from 'hamburger-react'
 
 const NavBar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -25,19 +25,16 @@ const NavBar = () => {
 
             {/* Mobile Menu Icon */}
             <div className="md:hidden">
-                <HiMenu onClick = {() => { console.log("button clicked") 
-                setMenuOpen(!menuOpen)
-                }} 
-                className="text-4xl text-white cursor-pointer" />
+                 <Hamburger toggled={menuOpen} toggle={setMenuOpen} size={28} color="#fff" />
 
                 {menuOpen && (
-                    <div>
-                            <h4>Home</h4>
-                            <h4>Features</h4>
-                            <h4>Notes</h4>
-                            <h4>Study Room</h4>
-                            <h4>Doubts</h4>
-                            <h4>Login / Sign Up</h4> 
+                    <div className="absolute top-16 right-4 w-full bg-gradient-to-br from-gray-900 via-black to-gray-800 rounded-lg p-6 shadow-xl flex flex-col gap-4 z-50">
+                            <h4 className="text-white text-center hover:text-indigo-400 transition cursor-pointer">Home</h4>
+                            <h4 className="text-white text-center hover:text-indigo-400 transition cursor-pointer">Features</h4>
+                            <h4 className="text-white text-center hover:text-indigo-400 transition cursor-pointer">Notes</h4>
+                            <h4 className="text-white text-center hover:text-indigo-400 transition cursor-pointer">Study Room</h4>
+                            <h4 className="text-white text-center hover:text-indigo-400 transition cursor-pointer">Doubts</h4>
+                            <button className="bg-gradient-to-r text-center from-purple-600 to-indigo-600 text-white rounded-lg py-2 mt-4 hover:scale-105 transition cursor-pointer">Login / Sign Up</button> 
                     </div>
                 )}
             </div>
