7a94b744136135b44f41326edf3f5e3e6db16828
Amrita <amritakataria515@gmail.com>
2025-06-25 23:44:57 +0530

    login page completed

--- studySync/src/App.jsx
+++ studySync/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className='min-h-screen bg-gradient-to-br from-black via-gray-900 to-indigo-900 text-white'>
        <Home/>
-       {/* <LogIn/> */}
+       <LogIn/>
        <SignUp/>
     </div>
   );
--- studySync/src/Components/LogIn.jsx
+++ studySync/src/Components/LogIn.jsx
@@ -1,23 +1,69 @@
+import {useState} from 'react';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+
 const LogIn = () => {
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const[showPassword, setShowPassword] = useState(false);
+  const[emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("")
+
+  const handleSubmit= (e) => {
+      e.preventDefault();
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+          setEmailError("Invalid email address");
+        } else{
+          setEmailError("");
+      } 
+     if (!password.trim()) {
+        setPasswordError("Password is required");
+        return;
+      }
+      
+      if (password.length < 6) {
+        setPasswordError("Password must be at least 6 characters");
+        return;
+      }
+      
+      setPasswordError("");
+    console.log("email is : " , email);
+    console.log("password is : ", password);
+  }
+
   return (
     <div className="min-h-screen flex justify-center items-center px-4">
       <div className="backdrop-blur-xl bg-white/10 border border-white/20 rounded-2xl shadow-2xl p-8 max-w-md w-full text-white">
         <h1 className="text-2xl font-semibold text-center mb-6">Log In</h1>
-
-        {/* Inputs + Button */}
-        <form className="w-full space-y-4">
+        <form className="w-full space-y-4" onSubmit={handleSubmit} >
           <input
             type="text"
             placeholder="Email"
-            name="email"
+            name = "email"
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
             className="w-full px-4 py-3 rounded-xl bg-white/10 text-white placeholder-gray-300 border border-white/20 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            className="w-full px-4 py-3 rounded-xl bg-white/10 text-white placeholder-gray-300 border border-white/20 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
+          {emailError && <p className="text-sm text-red-500 mt-1 ml-1">{emailError}</p>}
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-3 pr-12 rounded-xl bg-white/10 text-white placeholder-gray-300 border border-white/20 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-4 top-1/2 transform -translate-y-1/2 text-xl cursor-pointer text-white"
+             >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+          {passwordError && <p className="text-sm text-red-500 mt-1 ml-1">{passwordError}</p>}
+
           <button
             type="submit"
             className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 rounded-xl transition duration-300"
